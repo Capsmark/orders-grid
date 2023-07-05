13a40bde9db15eca565774743cb2a12986d37724
Amir <farhaadi.amir@gmail.com>
2023-07-05 13:13:52 +0330

    :lipstick: Updated SVG files used for percentage & hash

--- src/views/order/components/sltp-form-input.tsx
+++ src/views/order/components/sltp-form-input.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { FC } from 'react';
 
 interface SLTPProps {
@@ -42,45 +43,24 @@ export const SLTPFormInput: FC<SLTPProps> = ({
       />
 
       {isPercent && (
-        <div className='absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            stroke-width='2'
-            stroke='currentColor'
-            fill='none'
-            stroke-linecap='round'
-            stroke-linejoin='round'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-            <path d='M17 17m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
-            <path d='M7 7m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
-            <path d='M6 18l12 -12' />
-          </svg>
+        <div className='absolute inset-y-0 right-0 flex items-center pr-1.5 pointer-events-none'>
+          <Image
+            width={30}
+            height={30}
+            src={'/percentage.min.svg'}
+            alt='Percentage Icon Caps Order Grid'
+          ></Image>
         </div>
       )}
 
       {!isPercent && (
-        <div className='absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            stroke-width='2'
-            stroke='currentColor'
-            fill='none'
-            stroke-linecap='round'
-            stroke-linejoin='round'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-            <path d='M5 9l14 0' />
-            <path d='M5 15l14 0' />
-            <path d='M11 4l-4 16' />
-            <path d='M17 4l-4 16' />
-          </svg>
+        <div className='absolute inset-y-0 right-0 flex items-center pr-1.5 pointer-events-none'>
+          <Image
+            width={30}
+            height={30}
+            src={'/hash.min.svg'}
+            alt='Hash Icon Caps Order Grid'
+          ></Image>
         </div>
       )}
     </div>
