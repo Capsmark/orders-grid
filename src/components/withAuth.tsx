1c216a4baaf22c1d1c57f7a60069c9fd2c31ed31
Amir <farhaadi.amir@gmail.com>
2023-08-29 15:56:23 +0330

    🚀 Fix: fixed build errors for deploy

--- src/components/withAuth.tsx
+++ src/components/withAuth.tsx
@@ -1,14 +1,11 @@
 import useUserStore from '@/stores/user-store';
-
 import { useRouter } from 'next/router';
 import { ComponentType, FC, useEffect } from 'react';
 
-const withAuth =
-  <P extends object>(WrappedComponent: ComponentType<P>): FC<P> =>
-  (props: P) => {
+const withAuth = <P extends object>(WrappedComponent: ComponentType<P>): FC<P> => {
+  const AuthenticatedComponent: FC<P> = (props: P) => {
     // FIXME change the logic of store to use real auth instead of mocked email
     const { isLoggedIn } = useUserStore();
-
     const { push } = useRouter();
 
     useEffect(() => {
@@ -23,5 +20,15 @@ const withAuth =
 
     return null; // TODO add a <Loading /> component
   };
+
+  // Assign the display name to the returned component
+  AuthenticatedComponent.displayName = `WithAuth(${getDisplayName(WrappedComponent)})`;
+
+  return AuthenticatedComponent;
+};
+
+function getDisplayName(WrappedComponent: ComponentType<any>): string {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
 
 export default withAuth;
